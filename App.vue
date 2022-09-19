182ff5b281e3a3afa5b8a326585d4ef0339aea26
Lucas-Canyo <87463191+Lucas-Canyo@users.noreply.github.com>
2022-09-19 10:50:25 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,20 @@
+<script>
+	export default {
+		// 启动时
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		// 显示时
+		onShow: function() {
+			console.log('App Show')
+		},
+		// 退入后台时
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
